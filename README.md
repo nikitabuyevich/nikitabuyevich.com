733a37c57de9b50c9f976100af1c98e807d946c3
Nikita Buyevich <nikita.buyevich@gmail.com>
2016-02-05 00:30:28 -0600

    Update 2016-02-05T06:30:25.714Z

--- README.md
+++
@@ -1,8 +0,0 @@
-# [NikitaBuyevich.com](http://NikitaBuyevich.com)
-
-My personal website coded in: 
-* HTML
-* CSS
-* Bootstrap
-* Javascript
-* AngularJS
--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('nikitabuyevichCom', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
